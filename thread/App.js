f0210016780d0e5181a823906d06568715f3836c
jordanrios94 <jordanrios.software@gmail.com>
2018-06-09 16:23:41 +0100

    converted tabs to spaces

--- thread/App.js
+++ thread/App.js
@@ -17,68 +17,68 @@ require('firebase/firestore');
 
 class App extends Component {
 
-	state = {
-		isReady: false,
-		email: '',
-		password: ''
-	}
+    state = {
+        isReady: false,
+        email: '',
+        password: ''
+    }
 
-	constructor() {
-		super();
+    constructor() {
+        super();
 
-		const config = {
-			apiKey: "AIzaSyBOE0wGW9TRzP_InQz0cOnh-OR7SwyLM9w",
-			authDomain: "reactnative-auth-66287.firebaseapp.com",
-			databaseURL: "https://reactnative-auth-66287.firebaseio.com",
-			projectId: "reactnative-auth-66287",
-			storageBucket: "reactnative-auth-66287.appspot.com",
-			messagingSenderId: "381362358898"
-		};
-		
-		firebase.initializeApp(config);
-	}
+        const config = {
+            apiKey: "AIzaSyBOE0wGW9TRzP_InQz0cOnh-OR7SwyLM9w",
+            authDomain: "reactnative-auth-66287.firebaseapp.com",
+            databaseURL: "https://reactnative-auth-66287.firebaseio.com",
+            projectId: "reactnative-auth-66287",
+            storageBucket: "reactnative-auth-66287.appspot.com",
+            messagingSenderId: "381362358898"
+        };
+        
+        firebase.initializeApp(config);
+    }
 
-	startAsync = async () => {
-		try {
-			const email = await SecureStore.getItemAsync('email');
-			const password = await SecureStore.getItemAsync('password');
-			this.setState({ email, password });
-		} catch (error) {
-			return Promise.reject(error);
-		}
+    startAsync = async () => {
+        try {
+            const email = await SecureStore.getItemAsync('email');
+            const password = await SecureStore.getItemAsync('password');
+            this.setState({ email, password });
+        } catch (error) {
+            return Promise.reject(error);
+        }
 
-		return Promise.resolve(true);
-	}
+        return Promise.resolve(true);
+    }
   
-	render() {
-		if (!this.state.isReady) {
-			return (
-			  	<AppLoading
-					startAsync={this.startAsync}
-					onFinish={() => this.setState({ isReady: true })}
-					onError={console.warn}
-			  	/>
-			);
-		}
+    render() {
+        if (!this.state.isReady) {
+            return (
+                  <AppLoading
+                    startAsync={this.startAsync}
+                    onFinish={() => this.setState({ isReady: true })}
+                    onError={console.warn}
+                  />
+            );
+        }
 
-		const { email, password } = this.state;
-		const autoLogIn = email && password ? true : false;
-		const store = createStore(
-			reducers,
-			{ auth: { email, password, autoLogIn } },
-			applyMiddleware(ReduxThunk)
-		);
+        const { email, password } = this.state;
+        const autoLogIn = email && password ? true : false;
+        const store = createStore(
+            reducers,
+            { auth: { email, password, autoLogIn } },
+            applyMiddleware(ReduxThunk)
+        );
 
-		return (
-			<Provider store={store}>
-				<StyleProvider style={getTheme(platformStyles)}>
-					<Root>
-						<Router />
-					</Root>
-				</StyleProvider>
-			</Provider>
-		);
-	}
+        return (
+            <Provider store={store}>
+                <StyleProvider style={getTheme(platformStyles)}>
+                    <Root>
+                        <Router />
+                    </Root>
+                </StyleProvider>
+            </Provider>
+        );
+    }
 
 }
 
